6280a418f9274afdf23d484fcbde8419f2388978
ditss-labs <ditsslabs@gmail.com>
2026-01-07 21:09:09 +0700

    Create afk.js

---
+++ plugins/users/afk.js
@@ -0,0 +1,14 @@
+let afk = async (m, { conn: Ditss, text }) => {
+  let userdb = global.db.users[m.sender]
+  
+  userdb.afkTime = Date.now()
+  userdb.afkReason = text || 'Tanpa alasan'
+  
+  return m.reply(`😴 AFK aktif\n📌 Alasan: ${userdb.afkReason}`)
+}
+
+afk.help = ['afk [alasan]']
+afk.tags = ['main']
+afk.command = ['afk']
+
+export default afk
